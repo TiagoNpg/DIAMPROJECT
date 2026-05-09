d3481a56189d0bcbe71730449048b3e2b07a30fd
LEI-123298 <tmans1iscte@gmail.com>
2026-05-09 22:55:01 +0100

    More React config

---
+++ carmeets/carmeetsApp-frontend/src/components/Home.jsx
@@ -0,0 +1,24 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "reactstrap";
+//TODO: FAZER AQUI IMPLEMENTAÇÃO DO FRONTEND 
+/*function Home() {
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  );
+}
+function Content() {
+  return (
+    <Container style={{ marginTop: "20px", maxWidth: "800px" }}>
+      <Row>
+        <Col>
+          <QuestionTable />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+export default Home;
+*/
